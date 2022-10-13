a2c96c5f2c1e13ba83012dd06d66bb13cae018e7
Rodrigo Rivera <rodrigo.rivera.b@hotmail.com>
2022-10-13 14:16:42 -0300

    nueva propiedad, lazy loading img

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,3 +141,33 @@ function showSlides4(n) {
   slides[slideIndex4-1].style.display = "block";
   dots[slideIndex4-1].className += " active";
 }
+
+//carrousel 5
+let slideIndex5 = 1;
+showSlides5(slideIndex5);
+
+// Next/previous controls
+function plusSlides5(n) {
+  showSlides5(slideIndex5 += n);
+}
+
+// Thumbnail image controls
+function currentSlide5(n) {
+  showSlides5(slideIndex5 = n);
+}
+
+function showSlides5(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides5");
+  let dots = document.getElementsByClassName("dot5");
+  if (n > slides.length) {slideIndex5 = 1}
+  if (n < 1) {slideIndex5 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex5-1].style.display = "block";
+  dots[slideIndex5-1].className += " active";
+}
